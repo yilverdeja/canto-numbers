0a1e86cf7e6e199f070c033d5ef1e278f4518f53
yilverdeja <yilverdeja@users.noreply.github.com>
2023-09-18 16:00:19 +0800

    feat: add categories
    * categories in json file with information

--- src/pages/CategoriesPage.vue
+++ src/pages/CategoriesPage.vue
@@ -1,44 +1,9 @@
 <script setup lang="ts">
+import categoriesJson from "@/assets/data/categories.json"
 
 const router = useRouter()
 
-const categories = ref([
-    {
-        title: "Integers",
-        description: "Master positive whole numbers in Cantonese. From zero to a trillion, learn to pronounce and understand integers effortlessly",
-        url: "/integers",
-        published: true,
-        image: "/img/integers.png",
-    },
-    {
-        title: "Time",
-        description: "Explore the 12-hour format of time in Cantonese. Learn to tell time accurately and naturally, from morning to night",
-        url: "/time",
-        published: true,
-        image: "/img/time.png",
-    },
-    {
-        title: "Money",
-        description: "Count money with precision in Cantonese. Whether it's dollars or cents, master currency values with up to two decimal places",
-        url: "/money",
-        published: false,
-        image: "/img/money.png",
-    },
-    {
-        title: "Digits",
-        description: "Identify individual digits spoken consecutively, just like years or phone numbers. Enhance your digit recognition skills in Cantonese",
-        url: "/digits",
-        published: false,
-        image: "/img/digits.png",
-    },
-    {
-        title: "Dates",
-        description: "Understand how to express specific days in specific months in Cantonese. Master date formats and calendar-related vocabulary",
-        url: "/dates",
-        published: false,
-        image: "/img/dates.png",
-    },
-])
+const categories = ref(categoriesJson)
 
 // sort published first
 categories.value.sort((a, b) => {
@@ -57,6 +22,7 @@ const toCategory = (url, isPublished) => {
     <div class="flex flex-col h-screen">
         <PageHeader />
         <div class="max-w-[85rem] w-full mx-auto px-4 h-full flex flex-col py-4 sm:py-10">
+            <!-- title -->
             <div class="text-left md:text-center py-4 md:py-2 mb-2">
                 <h1 class="text-5xl md:text-6xl my-4">Categories</h1>
                 <h2 class="text-xl md:text-2xl font-light">Explore various numeral concept categories</h2>
